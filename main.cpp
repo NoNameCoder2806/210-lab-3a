4655b8b9ab37278477729f780ad9d2ee64e93b04
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-01 11:48:20 -0700

    Added function prototypes

--- main.cpp
+++ main.cpp
@@ -2,15 +2,21 @@
 #include <string>
 using namespace std;
 
+// Restaurant struct
 struct Restaurant
 {
-    string name;             // The name of the restaurant
-    string address;          // The address of the restaurant
-    string contactNumber;    // The contact number 
-    string email;            // The contact email
-    double rating;           // The rating of the restaurant
-    int yearEstablished;     // The year the restaurant was established
+    string name;              // The name of the restaurant
+    string address;           // The address of the restaurant
+    string contactNumber;     // The contact number 
+    string email;             // The contact email
+    double rating;            // The rating of the restaurant
+    int yearEstablished;      // The year the restaurant was established
 };
+
+// Function prototypes 
+Restaurant consoleInput();             // Let user input the data for the Restaurant struct object
+
+void displayStruct(Restaurant res);    // Display the data of the struct
 
 int main()
 {
