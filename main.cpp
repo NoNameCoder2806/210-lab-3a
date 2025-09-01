9b4f741aaf9be3efb295120964ea72cc7a1dbbae
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-01 11:37:01 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Restaurant
+{
+    string name;             // The name of the restaurant
+    string address;          // The address of the restaurant
+    string contactNumber;    // The contact number 
+    string email;            // The contact email
+    double rating;           // The rating of the restaurant
+    int yearEstablished;     // The year the restaurant was established
+};
+
+int main()
+{
+
+    return 0;
+}
