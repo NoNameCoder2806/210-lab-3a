493249c7f92673602ab9c9c77ae976e27b680fb7
NoNameCoder2806 <hoangvien2806@gmail.com>
2025-09-01 16:14:01 -0700

    Hard-coded 4 Restaurant, fixed the setw() formatting in the displayStruct(), created 4 Restaurant struct objects that receive user's input

--- main.cpp
+++ main.cpp
@@ -24,17 +24,63 @@ void displayStruct(Restaurant res);    // Display the data of the struct
 int main()
 {
     // Create 4 Restaurant structs by hard-coding the data 
-    Restaurant domino { "Domino's Pizza", "901 Sunvalley Blvd Ste 101, Concord, CA", "(925) 826-5040", 2.5, "American", "$", 1960 };
-    Restaurant phoSaiGon { "Pho Sai Gon City 2", "1617 Contra Costa Blvd, Pleasant Hill, CA" , "(925) 375-1353", 4.0, "Vietnamese", "$", 2018 };
-    Restaurant mcDonalds { "McDonald's", "624 Contra Costa Blvd, Pleasant Hill, CA", "(925) 677-2121", 2.5, "American", "$", 1940 };
+    Restaurant domino {    // Domino's Pizza
+        "Domino's Pizza",
+        "901 Sunvalley Blvd Ste 101, Concord, CA",
+        "(925) 826-5040",
+        2.5,
+        "American",
+        "$",
+        1960
+    };
+    displayStruct(domino);    // Display the Restaurant's information
+
+    Restaurant phoSaiGon {    // Pho Sai Gon city 2
+        "Pho Sai Gon city 2",
+        "1617 Contra Costa Blvd, Pleasant Hill, CA",
+        "(925) 375-1353",
+        4.0,
+        "Vietnamese",
+        "$",
+        2018
+    };
+    displayStruct(phoSaiGon);    // Display the Restaurant's information
+
+    Restaurant mcDonalds {    // McDonald's
+        "McDonald's",
+        "624 Contra Costa Blvd, Pleasant Hill, CA",
+        "(925) 677-2121",
+        2.5,
+        "American",
+        "$",
+        1940
+    };
+    displayStruct(mcDonalds);    // Display the Restaurant's information
+
+    Restaurant elAguila {    // El Aguila Mexican Cuisine
+        "El Aguila Mexican Cuisine",
+        "1300 Contra Costa Blvd, Ste 12, Pleasant Hill, CA 94523",
+        "(925) 682-4444",
+        4.5,
+        "Mexican",
+        "$$",
+        2013
+    };
+    displayStruct(elAguila);    // Display the Restaurant's information
 
     // Create 4 Restaurant structs by letting the user enter the data
-    Restaurant res0 = consoleInput();
+    cout << "Enter the first Restaurant: " << endl;    // First Restaurant
+    Restaurant res0 = consoleInput();    // Create a new Restaurant struct object
+    displayStruct(res0);                 // Display the Restaurant's information
 
-    // Display the hard-coded Restaurants
-    displayStruct(domino);
-    displayStruct(phoSaiGon);
-    displayStruct(mcDonalds);
+    Restaurant res1 = consoleInput();    // Create a new Restaurant struct object
+    displayStruct(res1);                 // Display the Restaurant's information
+
+    Restaurant res2 = consoleInput();    // Create a new Restaurant struct object
+    displayStruct(res2);                 // Display the Restaurant's information
+    
+    Restaurant res3 = consoleInput();    // Create a new Restaurant struct object
+    displayStruct(res3);                 // Display the Restaurant's information
 
     return 0;
 }
@@ -46,34 +92,37 @@ Restaurant consoleInput()
     Restaurant res;
 
     // Get the Restaurant's name
-    cout << "Enter the Restaurant's name: ";
+    cout << " - Enter the Restaurant's name: ";
     getline(cin, res.name);
 
     // Get the Restaurant's address
-    cout << "Enter the Restaurant's address: ";
+    cout << " - Enter the Restaurant's address: ";
     getline(cin, res.address);
 
     // Get the Restaurant's contact number
-    cout << "Enter the Restaurant's contact number: ";
+    cout << " - Enter the Restaurant's contact number: ";
     getline(cin, res.contactNumber);
 
     // Get the Restaurant's rating
-    cout << "Enter the Restaurant's rating (0.0 to 5.0): ";
+    cout << " - Enter the Restaurant's rating (0.0 to 5.0): ";
     cin >> res.rating;
     cin.ignore(1000, 10);
 
     // Get the Restaurant's cuisine type
-    cout << "Enter the Restaurant's cuisine type (eg. Vietnamese, American, etc): ";
+    cout << " - Enter the Restaurant's cuisine type (eg. Vietnamese, American, etc): ";
     getline(cin, res.cuisineType);
 
     // Get the Restaurant's price range
-    cout << "Enter the Restaurant's price range (eg. $, $$ or $$$): ";
+    cout << " - Enter the Restaurant's price range (eg. $, $$ or $$$): ";
     getline(cin, res.priceRange);
 
     // Get the Restaurant's established year
-    cout << "Enter the Restaurant's established year: ";
+    cout << " - Enter the Restaurant's established year: ";
     cin >> res.yearEstablished;
     cin.ignore(1000, 10);
+
+    // Enter a new line
+    cout << endl;
 
     // Return the Restaurant struct object
     return res;
@@ -82,13 +131,13 @@ Restaurant consoleInput()
 void displayStruct(Restaurant res)
 {
     // Display the Restaurant struct object's information
-    cout << setw(32) << "Restaurant's name: " << res.name << endl;
-    cout << setw(32) << "Restaurant's address: " << res.address << endl;
-    cout << setw(32) << "Restaurant's contact number: " << res.contactNumber << endl;
-    cout << setw(32) << "Restaurant's rating: " << res.rating << endl;
-    cout << setw(32) << "Restaurant's cuisine type: " << res.cuisineType << endl;
-    cout << setw(32) << "Restaurant's price range: " << res.priceRange << endl;
-    cout << setw(32) << "Restaurant's established year: " << res.yearEstablished << endl;
+    cout << left << setw(32) << "Restaurant's name: " << res.name << endl;
+    cout << left << setw(32) << "Restaurant's address: " << res.address << endl;
+    cout << left << setw(32) << "Restaurant's contact number: " << res.contactNumber << endl;
+    cout << left << setw(32) << "Restaurant's rating: " << res.rating << endl;
+    cout << left << setw(32) << "Restaurant's cuisine type: " << res.cuisineType << endl;
+    cout << left << setw(32) << "Restaurant's price range: " << res.priceRange << endl;
+    cout << left << setw(32) << "Restaurant's established year: " << res.yearEstablished << endl;
     
     // Enter a new line after displaying the data
     cout << endl;
